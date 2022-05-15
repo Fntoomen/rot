a95ffb64bf76a394f7584839b44f6e533fdaa595
Mikołaj Lubiak <galanonim@disroot.org>
2022-05-15 16:45:47 +0200

    Made version working with wordlist. Fixed cesar.py to rot.py in breakrot.py.

--- breakrot.py
+++ breakrot.py
@@ -1,18 +1,20 @@
-from cesar import decrypt as dec
-from cesar import chars
+from rot import decrypt as dec
+from rot import chars
 from sys import argv
 from collections import Counter
 
-def brkc(m: str, t: tuple):
+def brkc(m: str, f: str):
+    t=tuple(open(f).read().splitlines()[:1000])
     decm=[]
     for i in range(1, len(chars)):
         decmsg=dec(m, i)
         print(f"{i}: {decmsg}")
         for x in range(len(decmsg)):
-            for y in range(len(decmsg)):
-                if decmsg[x:y] in t:
+            for y in range(x, len(decmsg)):
+                if decmsg[x:y].lower() in t:
                         decm.append(decmsg)
     return Counter(decm)
-a=argv[1]
-b=argv[2].split(" ")
-print(brkc(a, b))
+if __name__ == "__main__":
+    a=argv[1]
+    b=argv[2]
+    print(brkc(a, b))
